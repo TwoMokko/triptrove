a5b085a13dda31a993591447c23b15f21198c042
twoMokko <two.mokko@gmail.com>
2025-05-30 22:14:57 -0700

    немного изменена логика редактирования поля (при наведении появляется иконка карандаш для редактирования), добавлена проверка (если поле не изменилось)

--- resources/js/pages/profile/ProfilePage.vue
+++ resources/js/pages/profile/ProfilePage.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
-import { computed, ComputedRef, onMounted, onUnmounted, ref } from 'vue'
+import {computed, ComputedRef, onMounted, onUnmounted, ref, watch} from 'vue'
 import Loader from "@/shared/ui/Loader.vue"
 import { useUsersStore } from "@/etities/user"
 import FileUploader from "@/shared/ui/FileUploader.vue"
+import Icon from "@/shared/ui/Icon.vue"
+import { mdiPencil } from '@mdi/js'
 
 interface EditMode {
     name: boolean,
@@ -22,11 +24,24 @@ const inputRefs = ref<Record<string, HTMLInputElement | null>>({
     login: null,
 })
 
-const currentUser = computed(() => usersStore.currentUser)
-const isLoading: ComputedRef<boolean> = computed(() => {
-    return usersStore.currentUser === null
+const originalValues = ref<Record<string, string>>({
+    name: '',
+    email: '',
+    login: ''
 })
 
+const currentUser = computed(() => usersStore.currentUser)
+const isLoading: ComputedRef<boolean> = computed(() =>  usersStore.currentUser === null)
+
+watch(currentUser, (newVal: typeof currentUser.value) => {
+    if (newVal) {
+        originalValues.value = {
+            name: newVal.name,
+            email: newVal.email,
+            login: newVal.login
+        }
+    }
+}, { immediate: true })
 
 const handleClickOutside = (event: MouseEvent) => {
     const isOutsideAllInputs = Object.values(inputRefs.value).every(
@@ -36,7 +51,12 @@ const handleClickOutside = (event: MouseEvent) => {
     if (isOutsideAllInputs) {
         Object.keys(editMode.value).forEach((key) => {
             if (editMode.value[key]) {
-                onSave(key)
+                const field = key as keyof EditMode
+
+                currentUser.value[field] !== originalValues.value[field]
+                    ? onSave(field)
+                    : editMode.value[field] = false
+
             }
             editMode.value[key] = false
         })
@@ -44,12 +64,28 @@ const handleClickOutside = (event: MouseEvent) => {
 }
 
 const onSave = (field: keyof EditMode) => {
-    console.log(`Сохранено поле ${field}:`, currentUser.value[field])
-    editMode.value[field] = false
+    try {
+        // Здесь логика сохранения в API
+        // Например:
+        // await usersStore.updateUser({
+        //   [field]: currentUser.value[field]
+        // })
+
+        console.log(`Сохранено поле ${field}:`, currentUser.value[field])
+        originalValues.value[field] = currentUser.value[field]
+    } catch (error) {
+        console.error('Ошибка при сохранении:', error)
+        // Откат значения, если сохранение не удалось
+        currentUser.value[field] = originalValues.value[field]
+    } finally {
+        editMode.value[field] = false
+    }
 }
 
 onMounted(() => document.addEventListener('click', handleClickOutside))
 onUnmounted(() => document.removeEventListener('click', handleClickOutside))
+
+//TODO: оптимизировать (создать компонент для строк с данными)
 </script>
 
 <template>
@@ -63,8 +99,8 @@ onUnmounted(() => document.removeEventListener('click', handleClickOutside))
                 :src="currentUser.avatar ? `storage/${currentUser.avatar}` : '/storage/users/avatars/default-user.svg'"
                 :class-name="'w-40 h-40 rounded-full'"
             />
-            <div>
-                <h1 class="text-2xl mb-4 flex gap-2 items-center">
+            <div class="w-96">
+                <h1 class="text-2xl mb-4 flex gap-2 items-center group">
                     <span>Имя:</span>
 
                     <input
@@ -74,16 +110,26 @@ onUnmounted(() => document.removeEventListener('click', handleClickOutside))
                         :type="'text'"
                         :ref="(el) => (inputRefs.name = el as HTMLInputElement)"
                         @keyup.enter="editMode.name = false; onSave('name')"
+                        class="w-full"
                     />
-                    <span
+                    <div
                         v-else
-                        @click.stop="editMode.name = !editMode.name"
+                        class="w-full flex gap-2 justify-between"
                     >
-                        {{ currentUser.name }}
-                    </span>
+                        <span>{{ currentUser.name }}</span>
+                        <span
+                            @click.stop="editMode.name = !editMode.name"
+                            class="hidden group-hover:inline-block cursor-pointer"
+                        >
+                            <Icon
+                                :iconPath="mdiPencil"
+                                class="w-4 h-4 text-secondary hover:text-dark"
+                            />
+                        </span>
+                    </div>
                 </h1>
                 <div>id: {{ currentUser.id }}</div>
-                <div class="flex gap-2 items-center">
+                <div class="flex gap-2 items-center group">
                     login:
 
                     <input
@@ -93,15 +139,25 @@ onUnmounted(() => document.removeEventListener('click', handleClickOutside))
                         :type="'text'"
                         :ref="(el) => (inputRefs.login = el as HTMLInputElement)"
                         @keyup.enter="editMode.login = false; onSave('login')"
+                        class="w-full"
                     />
-                    <span
+                    <div
                         v-else
-                        @click.stop="editMode.login = !editMode.login"
+                        class="w-full flex gap-2 justify-between"
                     >
-                        {{ currentUser.login }}
-                    </span>
+                        <span>{{ currentUser.login }}</span>
+                        <span
+                            @click.stop="editMode.login = !editMode.login"
+                            class="hidden group-hover:inline-block cursor-pointer"
+                        >
+                            <Icon
+                                :iconPath="mdiPencil"
+                                class="w-4 h-4 text-secondary hover:text-dark"
+                            />
+                        </span>
+                    </div>
                 </div>
-                <div class="flex gap-2 items-center">
+                <div class="flex gap-2 items-center group">
                     email:
 
                     <input
@@ -111,13 +167,23 @@ onUnmounted(() => document.removeEventListener('click', handleClickOutside))
                         :type="'text'"
                         :ref="(el) => (inputRefs.email = el as HTMLInputElement)"
                         @keyup.enter="editMode.login = false; onSave('email')"
+                        class="w-full"
                     />
-                    <span
+                    <div
                         v-else
-                        @click.stop="editMode.email = !editMode.email"
+                        class="w-full flex gap-2 justify-between"
                     >
-                        {{ currentUser.email }}
-                    </span>
+                        <span>{{ currentUser.email }}</span>
+                        <span
+                            @click.stop="editMode.email = !editMode.email"
+                            class="hidden group-hover:inline-block cursor-pointer"
+                        >
+                            <Icon
+                                :iconPath="mdiPencil"
+                                class="w-4 h-4 text-secondary hover:text-dark"
+                            />
+                        </span>
+                    </div>
                 </div>
             </div>
         </div>
